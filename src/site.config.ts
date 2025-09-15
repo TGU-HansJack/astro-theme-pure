34b6aa351108b9efd0483e0b841e41ce6daed503
TGU-HansJack <2310210201@tiangong.edu.cn>
2025-09-15 18:19:18 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'HansJack',
+  title: 'Hans Jack',
   /** Will be used in index page & copyright declaration */
   author: 'Hans J. Han',
   /** Description metadata for your website. Can be used in page metadata. */
